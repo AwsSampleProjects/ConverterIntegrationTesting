2adc67d9784aecfd2c134a115696670fd9f837bd
Lukasz Taraszka <l.taraszka@luktar.ovh>
2025-06-04 23:43:35 +0200

    Enhance init-localstack.sh by adding queue creation, bucket initialization, and notification configuration, along with a queue purge command for test messages.

--- Scripts/init-localstack.sh
+++ Scripts/init-localstack.sh
@@ -4,8 +4,10 @@ AWS_REGION="eu-central-1"
 AWS_BUCKET="converter-bucket"
 AWS_QUEUE="converter-queue"
 
+# Create queue
 awslocal sqs create-queue --queue-name "$AWS_QUEUE" --region "$AWS_REGION"
 
+# Initialize queue ARN
 QUEUE_ARN=$(awslocal sqs get-queue-attributes \
     --queue-url http://localhost:4566/000000000000/"$AWS_QUEUE" \
     --attribute-names QueueArn \
@@ -13,8 +15,10 @@ QUEUE_ARN=$(awslocal sqs get-queue-attributes \
     --output text \
     --region "$AWS_REGION")
 
+# Create bucket
 awslocal s3 mb s3://"$AWS_BUCKET" --region "$AWS_REGION"
 
+# Set bucket notification configuration
 awslocal s3api put-bucket-notification-configuration \
     --bucket "$AWS_BUCKET" \
     --notification-configuration '{
@@ -35,3 +39,6 @@ awslocal s3api put-bucket-notification-configuration \
             }
         ]
     }'
+
+# Clear queue with test messages
+awslocal sqs purge-queue --queue-url http://localhost:4566/000000000000/"$AWS_QUEUE" --region "$AWS_REGION"
