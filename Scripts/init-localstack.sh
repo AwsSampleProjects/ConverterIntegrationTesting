29495445ae887a0b5a1075cc45a4ee10c1abeccb
Lukasz Taraszka <l.taraszka@luktar.ovh>
2025-06-04 23:24:59 +0200

    Add Docker Compose setup with Postgres and LocalStack services, create .env.example, and add initialization scripts

---
+++ Scripts/init-localstack.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+AWS_REGION="eu-central-1"
+AWS_BUCKET="converter-bucket"
+AWS_QUEUE="converter-queue"
+
+awslocal sqs create-queue --queue-name "$AWS_QUEUE" --region "$AWS_REGION"
+
+QUEUE_ARN=$(awslocal sqs get-queue-attributes \
+    --queue-url http://localhost:4566/000000000000/"$AWS_QUEUE" \
+    --attribute-names QueueArn \
+    --query 'Attributes.QueueArn' \
+    --output text \
+    --region "$AWS_REGION")
+
+awslocal s3 mb s3://"$AWS_BUCKET" --region "$AWS_REGION"
+
+awslocal s3api put-bucket-notification-configuration \
+    --bucket "$AWS_BUCKET" \
+    --notification-configuration '{
+        "QueueConfigurations": [
+            {
+                "QueueArn": "'$QUEUE_ARN'",
+                "Events": ["s3:ObjectCreated:*"]
+            }
+        ]
+    }'
